28f308da01a5f57fd44ebe177fefeacf22d115be
TrifShterev <tshterev@yahoo.com>
2020-11-01 15:34:46 +0200

    Add project files.

---
+++ Sort Persons by Name and Age/Person.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firsName, string lastName, int age, decimal salary)
+        {
+            FirstName = firsName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+                
+            }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value;
+            }
+        }
+
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{FirstName} {LastName} receives {Salary:f2} leva.");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
